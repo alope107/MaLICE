87f6f325a4af900ac47b26d114ba32a8c83a0c8f
Auberon López <auberonlopez@gmail.com>
2020-04-02 17:54:05 -0700

    First test case

---
+++ src/test/test_optimizer.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+import pandas as pd
+
+from malice.optimizer import MaliceOptimizer
+
+def _test_residues():
+    return pd.DataFrame( {
+        "residue" : [],
+        "15N" : [],
+        "1H" : [],
+        "intensity" : [],
+        "titrant" : [],
+        "visible" : []
+    })
+
+def _test_object():
+    return MaliceOptimizer(data=_test_residues())
+
+class TestOptimizer(TestCase):
+
+    def test_set_bounds(self):
+        optimizer = _test_object()
+        bounds = ([1, 2, 3], [4, 5, 6])
+        optimizer.set_bounds(bounds)
+        self.assertEqual(bounds,optimizer.get_bounds())
